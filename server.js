d3ca4113b07e613c564d3cb266a273ce5b0b60da
hpsingh546 <harmanreetsingh546@gmail.com>
2026-02-23 00:00:15 +0530

    chapter 3 part 2 system persona

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import 'dotenv/config'
 const groq = new Groq({ apiKey:process.env.GROQ_API_KEY});
 async function main() {
     const response=await groq.chat.completions.create({
-    messages: [{ content: 'explain how many country in the world', role: 'user'}],
+    messages: [
+    { content: 'who are you', role: 'user'},
+    { content: 'you are world best comedian create your name for yourself but not your model name, give humour in each answer and be polite and speak truth', role: 'system'}//system persona
+
+],
     model: 'meta-llama/llama-4-scout-17b-16e-instruct',
     },
     )
